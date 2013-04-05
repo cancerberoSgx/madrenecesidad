a4bdd776baf2133fe04433bfff148c2dd8b78641
cancerberoSgx <sebastigurin@gmail.com>
2013-04-05 12:20:38 -0300

    initial contrib

---
+++ madrenecesidad/src/org/sgx/madrenecesidad/client/service/UserService.java
@@ -0,0 +1,11 @@
+package org.sgx.madrenecesidad.client.service;
+
+import org.sgx.madrenecesidad.client.model.UserStateModel;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+@RemoteServiceRelativePath("userService")
+public interface UserService extends RemoteService {
+	UserStateModel getUserState();
+}
---
+++ madrenecesidad/src/org/sgx/madrenecesidad/client/service/UserServiceAsync.java
@@ -0,0 +1,11 @@
+package org.sgx.madrenecesidad.client.service;
+
+import org.sgx.madrenecesidad.client.model.UserStateModel;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public interface UserServiceAsync {
+
+	void getUserState(AsyncCallback<UserStateModel> callback);
+
+}
